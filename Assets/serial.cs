7b1af4c20b6d6eeac86f7b874d1c5e66b4e1f4ec
kruko32 <48065875+kruko32@users.noreply.github.com>
2019-05-05 19:05:00 +0200

    UART
    prace nad sterowaniem z arduino

---
+++ Assets/serial.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO.Ports;
+
+public class serial : MonoBehaviour
+{
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
--- Assets/sterowanie.cs
+++ Assets/sterowanie.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.IO.Ports;
+
 
 public class sterowanie : MonoBehaviour
 {
@@ -14,8 +16,6 @@ public class sterowanie : MonoBehaviour
     public Text wychylenieAktHUD;
     public Text COGAktHUD;
     public Text ROTAktHUD;
-    public RawImage north;
-
 
     public float silnik = 0.0f;
     public float pletwaAkt = 0.0f;
@@ -26,24 +26,30 @@ public class sterowanie : MonoBehaviour
     public const float wychylenieMax = 35.0f;
     public float ROT = 0.0f;
     public float COG = 0.0f;
-
-
     public float K = 0.07f;
+    public string test = "test";
+
+    void Start()
+    {
+        gameObject.GetComponent<ArduinoConnector>().WriteToArduino(test);
+    }
 
 
     void Update()
     {
+        
         predkosc.text = predkoscNastawa.value.ToString() + " %";
         pletwa.text = pletwaNastawa.value.ToString() + " °";
         
-
         if (predkoscAktualna != predkoscNastawa.value * predkoscMax / 100)
         {
             predkoscAktualna = Mathf.Lerp(predkoscAktualna, (predkoscMax * predkoscNastawa.value) / 100, 0.05f * Time.deltaTime);
             predkoscAktHUD.text = predkoscAktualna.ToString("0.0") + " kn";
         }
+
         pletwaAkt = Mathf.MoveTowards(pletwaAkt, pletwaNastawa.value, 1f * Time.deltaTime);
         predkoscAktualna = Mathf.MoveTowards(predkoscAktualna, (predkoscMax * predkoscNastawa.value) / 100, 0.05f * Time.deltaTime);
+
         if (predkoscAktualna != 0)
         {
             ROT += (K * (pletwaAkt) - ROT) * Time.deltaTime;
@@ -52,17 +58,15 @@ public class sterowanie : MonoBehaviour
             COG += ROT * Time.deltaTime;
             COGAktHUD.text = COG.ToString("0.0") + "  °";
             {
-
                 x += predkoscAktualna * Time.deltaTime * Mathf.Sin(COG * Mathf.PI / 180);
                 z += predkoscAktualna * Time.deltaTime * Mathf.Cos(COG * Mathf.PI / 180);
                 transform.position = new Vector3(2100+z, 500, 1900-x);
                 transform.rotation = Quaternion.Euler(0, COG, 0);
-
-                GameObject.Find("Canvas/RawImage").transform.position = new Vector3(536.4315f-(COG*8), (453.55835f), 0);
-
                 wychylenieAktHUD.text = pletwaAkt.ToString("0") + " °";
             }
         }
+
+
     }
 }
 
